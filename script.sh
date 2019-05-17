4c05e98450a7ab0b289b5a55db9e4faadd133466
Victor Agababov <vagababov@gmail.com>
2019-05-17 14:38:04 -0400

    add comments

--- script.sh
+++ script.sh
@@ -9,6 +9,7 @@ NC='\033[0m'
 kubectl delete ksvc prime-ui prime-api
 clear
 
+# 1. Deploy the positive HTTP backend and front end.
 echo -e "${RED}The simple HTTP API backend${WT}\n"
 
 cat http_service.yaml
@@ -27,6 +28,7 @@ read -p "====="
 
 ko apply -f ../prime-client/ui.yaml
 
+# 2. Display the generated endpoint and probe for readiness.
 echo -e "${RED}Examine the Knative Services...${WT}\n"
 
 kubectl get ksvc
@@ -42,8 +44,11 @@ done
 
 echo -e " done.\n"
 
+# 3. Browser page demo here...
 read -p "====="
 
+# 4. Deploy the negative backend; show that's it's balanced 60/40
+#    In real life would be 1/99...
 echo -e "${RED}We developed a new negative backend${WT}\n"
 
 cat ./http_service3.yaml
@@ -54,8 +59,10 @@ echo -e "${RED}Deploying new version, 60/40 split${WT}\n"
 
 ko apply -f ./http_service3.yaml
 
+# 4.5 Browser demo here to show negative results.
 read -p "====="
 
+# 5. Scaling exercise. Launch hey. In the background.
 echo -e "${RED}Now, let us see how this app scales${WT}\n"
 
 read -p "====="
@@ -64,6 +71,7 @@ hey -t 90 -n 100000 -c 350 "http://prime-ui.dangerd.net/prime?query=611101"&
 
 read -p "===== hey launched..."
 
+# 6. gRPC backend is now added. Has 0 traffic coming to it.
 echo -e "${RED}Now we decided to develop a gRPC backend${WT}\n"
 
 read -p "====="
@@ -76,10 +84,12 @@ ko apply -f ./grpc_service.yaml
 
 read -p "====="
 
+# 6.5. Show the endpoints, obviously cannot be used.
 echo -e "${RED}Checking the endpoints...${WT}\n"
 
 kubectl get ksvc prime-api -oyaml | grep "url:"
 
+# 7. new UI client will load balance over gRPC and HTTP seamlessly.
 read -p "====="
 
 echo -e "${RED}The load balanced HTTP and gRPC UI Client${WT}\n"
@@ -92,5 +102,6 @@ ko apply -f ../prime-client/ui3.yaml
 
 read -p "====="
 
+# 8. Browser demo with gRPC.
 echo -e "\n\t\t\t\t${NC}That's all folks!\n"
 
